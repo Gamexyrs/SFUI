eb75d2f6b6a64dc00ce7c6e43f85a042f6e5a523
Gamexyrs <100008604+Gamexyrs@users.noreply.github.com>
2024-07-22 23:19:36 +0800

    Add files via upload

---
+++ include/SFUI/Graphics.hpp
@@ -0,0 +1,111 @@
+#ifndef __SFUI_INCLUDE_GRAPHICS__
+  #if __has_include("./Graphics/Object.hpp")
+    #include "./Graphics/Object.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/Object.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Buildable.hpp")
+    #include "./Graphics/Buildable.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/Buildable.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/MoveMethod.hpp")
+    #include "./Graphics/MoveMethod.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/MoveMethod.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Movable.hpp")
+    #include "./Graphics/Movable.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/Movable.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Textable.hpp")
+    #include "./Graphics/Textable.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/Textable.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Touchable.hpp")
+    #include "./Graphics/Touchable.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/Touchable.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Div/Div.hpp")
+    #include "./Graphics/Div/Div.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/Div.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Div/TextDiv.hpp")
+    #include "./Graphics/Div/TextDiv.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/TextDiv.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Div/MessageDiv.hpp")
+    #include "./Graphics/Div/MessageDiv.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/MessageDiv.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Div/ProgressDiv.hpp")
+    #include "./Graphics/Div/ProgressDiv.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/ProgressDiv.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/AbstractButton.hpp")
+    #include "./Graphics/Button/AbstractButton.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/AbstractButton.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/PushButton.hpp")
+    #include "./Graphics/Button/PushButton.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/PushButton.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/CheckButton.hpp")
+    #include "./Graphics/Button/CheckButton.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/CheckButton.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/AbstractList_CheckButtons.hpp")
+    #include "./Graphics/Button/AbstractList_CheckButtons.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/AbstractList_CheckButtons.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/List_RadioButtons.hpp")
+    #include "./Graphics/Button/List_RadioButtons.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/List_RadioButtons.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/List_CheckButtons.hpp")
+    #include "./Graphics/Button/List_CheckButtons.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/List_CheckButtons.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Button/InputDiv.hpp")
+    #include "./Graphics/Button/InputDiv.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/InputDiv.hpp"
+  #endif
+  
+  #if __has_include("./Graphics/Div/InputMessageDiv.hpp")
+    #include "./Graphics/Div/InputMessageDiv.hpp"
+  #else
+    #error "No such file: SFUI/Graphics/InputMessageDiv.hpp"
+  #endif
+  
+  #define __SFUI_INCLUDE_GRAPHICS__ 18
+#endif
